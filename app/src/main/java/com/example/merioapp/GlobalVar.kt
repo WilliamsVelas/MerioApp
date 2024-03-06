b6b43b461a675c3ce712a7eb542dd6a8e5628473
Williams Velasquez <williams.velasquez.rivas@gmail.com>
2024-03-06 10:06:14 -0400

    object global var was added

---
+++ app/src/main/java/com/example/merioapp/GlobalVar.kt
@@ -0,0 +1,8 @@
+package com.example.merioapp
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+
+object GlobalVar {
+    var amountConvertion by mutableStateOf<Float>(0.0f)
+}
